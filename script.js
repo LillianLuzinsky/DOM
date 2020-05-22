9cf317cc1c0eaf6aca13feb9131d4bc55d1d0841
Lillian Luzinsky <eyeris@tpg.com.au>
2020-05-22 16:08:52 +1000

    learning about document.all

---
+++ script.js
@@ -0,0 +1,20 @@
+let val;
+
+val = document;
+val = document.all;//this gives us a collection
+val = document.all[0];//html
+val = document.all[1];//head
+val = document.all[2];//meta charset
+val = document.all.length;//43
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+
+
+
+console.log(val);
